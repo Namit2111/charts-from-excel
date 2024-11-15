640b5c3b1a12ceae54a7e249d226fa1afe58eef6
Nmait2111 <namitjain2111@gmail.com>
2024-11-15 14:31:41 +0530

    added supprt for serving build files

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,22 @@ const express = require('express');
 const fileRoutes = require('./routes/fileRoutes');
 const sheetsRoutes = require('./routes/sheetsRoutes');
 const cors = require('cors');
+const path = require('path'); 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 // API routes
 app.use('/api', fileRoutes);
 app.use('/api', sheetsRoutes);
 
+  // Handle all routes that don't match API routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
 // Basic error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
